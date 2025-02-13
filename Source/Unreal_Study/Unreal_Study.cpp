4bb36792ad8024f76b9b88f5a864a6bdc5f152e6
Jordy <jordyjordy@naver.com>
2025-02-13 10:33:50 +0900

    Delete Source directory

--- Source/Unreal_Study/Unreal_Study.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Unreal_Study.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unreal_Study, "Unreal_Study" );
